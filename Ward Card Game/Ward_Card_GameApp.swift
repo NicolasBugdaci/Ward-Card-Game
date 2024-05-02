77ec8016010fbc95c8a89404e92bf4eac8b76cdb
CheopsCode <nicolas@bugdaci.eu>
2024-05-02 20:54:36 +0200

    Initial Commit

---
+++ Ward Card Game/Ward_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ward_Card_GameApp.swift
+//  Ward Card Game
+//
+//  Created by Nicolas Bugdaci on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Ward_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
