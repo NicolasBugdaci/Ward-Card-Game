4f0fcfaac390080e2a1441f5f44f199136ddbcf4
CheopsCode <nicolas@bugdaci.eu>
2024-05-03 10:48:06 +0200

    Wild Card Game
    First application to refresh knowledge:
    
    - String, Int
    - Booleans
    - Functions
    - Conditions
    - Statements
    - XCode

--- Ward Card Game/ContentView.swift
+++ Ward Card Game/ContentView.swift
@@ -8,15 +8,113 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card3"
+    @State var cpuCard = "card2"
+    @State var playerScore  = 0
+    @State var cpuScore = 0
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        ZStack {
+            Image ("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                
+                Spacer()
+                
+                //
+                Button(action: deal, label: {
+                    Image("button")
+                })
+                //
+                
+                
+                Spacer()
+                
+                
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                        
+                    }
+                    
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                        
+                    }
+                    Spacer()
+                }.foregroundColor(.white)
+                
+                Spacer()
+            }
+            
+            
+            
+        } .background()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
+    func deal () {
+        // Randomize the players cards
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomize the cpus cards
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the scores
+        if playerCardValue > cpuCardValue {
+            // add +1 to Player
+            playerScore += 1
+        } else if cpuCardValue > playerCardValue {
+            // add +1 to CPU
+            cpuScore += 1
+        } else {
+            // do nothing
+        }
+    }
+    
+    
+    
 }
 
 #Preview {
